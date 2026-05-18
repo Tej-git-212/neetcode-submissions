1afe2b345b1ba46c16d73f503d26570806fafc1a
Tejaswini AVSV <69730029+Tej-git-212@users.noreply.github.com>
2026-05-18 23:15:39 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int ls = s.length(), lt = t.length();
+        if(ls != lt) return false;
+
+        int[] hs = new int[26];
+
+        for(int i = 0; i < ls; i++) {
+            hs[s.charAt(i)-'a'] += 1;
+        }
+        for(int i = 0; i < lt; i++) {
+            if(hs[t.charAt(i)-'a'] < 0) return false;
+            hs[t.charAt(i)-'a'] -= 1;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(hs[i] != 0) return false;
+        }
+        return true;
+    }
+}
